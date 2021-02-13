98e22c624fefe245319a84e33ce9b33529f76e8f
Nick Bence <nick.bence@dynatrace.com>
2021-02-12 22:17:49 -0500

    cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -29,6 +29,31 @@ db.once('open', () => {
   console.log('Connected to Database');
 });
 
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  //res.setHeader("Access-Control-Allow-Credentials");
+
+  // Pass to next layer of middleware
+  return next();
+});
+
 // SERVER
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
